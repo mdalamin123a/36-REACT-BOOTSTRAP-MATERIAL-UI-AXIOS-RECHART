41a8ddc45f6aa251d5055d02948bc69ed371fcb4
mdalamin123a <md.alamindev90@gmail.com>
2024-10-27 10:15:52 +0600

    36-7-Getting-Started-With-Material-UI

---
+++ src/components/News/News.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Card, CardMedia, CardContent, Typography, CardActions, Button} from '@mui/material';
+
+const News = () => {
+    return (
+        <Card sx={{ maxWidth: 345, backgroundColor: 'red' }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    );
+};
+
+export default News;
